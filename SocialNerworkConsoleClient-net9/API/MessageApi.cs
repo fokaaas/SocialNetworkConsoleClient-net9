1a4f359cb1aff8bf5aed180b902efec939d406fa
Stanislav Basarab <stanislav.basarab@infotech.gov.ua>
2024-12-23 21:10:50 +0200

    feat(API): implemented API client for messages

---
+++ SocialNerworkConsoleClient-net9/API/MessageApi.cs
@@ -0,0 +1,35 @@
+using SocialNerworkConsoleClient_net9.Interfaces;
+using SocialNerworkConsoleClient_net9.Models.Message;
+
+namespace SocialNerworkConsoleClient_net9.API;
+
+public class MessageApi : ClientApi, IMessageApi
+{
+    public MessageApi(string baseUrl) : base(baseUrl)
+    {
+    }
+
+    public async Task<MessageModel> GetById(int id)
+    {
+        var endpoint = $"messages/{id}";
+        return await GetAsync<MessageModel>(endpoint);
+    }
+
+    public async Task Create(MessageCreateModel messageModel)
+    {
+        var endpoint = "messages";
+        await PostAsync<MessageCreateModel, object>(endpoint, messageModel);
+    }
+
+    public async Task Update(int id, MessageUpdateModel messageModel)
+    {
+        var endpoint = $"messages/{id}";
+        await PatchAsync<MessageUpdateModel, object>(endpoint, messageModel);
+    }
+
+    public async Task Delete(int id)
+    {
+        var endpoint = $"messages/{id}";
+        await DeleteAsync(endpoint);
+    }
+}
---
+++ SocialNerworkConsoleClient-net9/Interfaces/IMessageApi.cs
@@ -0,0 +1,14 @@
+using SocialNerworkConsoleClient_net9.Models.Message;
+
+namespace SocialNerworkConsoleClient_net9.Interfaces;
+
+public interface IMessageApi
+{
+    Task<MessageModel> GetById(int id);
+        
+    Task Create(MessageCreateModel messageModel);
+        
+    Task Update(int id, MessageUpdateModel messageModel);
+        
+    Task Delete(int id);
+}
