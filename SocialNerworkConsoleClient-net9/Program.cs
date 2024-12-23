4804f8e5d7ea50fef87c398398ddf2ae827e0769
Stanislav Basarab <stanislav.basarab@infotech.gov.ua>
2024-12-23 21:51:55 +0200

    enhancement(Program): added console skeleton

--- SocialNerworkConsoleClient-net9/Program.cs
+++ SocialNerworkConsoleClient-net9/Program.cs
@@ -3,5 +3,29 @@ while (true)
 {
     Console.Write("> ");
     var input = Console.ReadLine();
+    
     if (input is null) continue;
+    if (input == "exit")
+    {
+        Console.WriteLine("Bye!");
+        break;
+    };
+
+    try
+    {
+        GetCommand(input.Trim())();
+    }
+    catch (Exception e)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(e.Message);
+        Console.ResetColor();
+    }
 }
+
+Command GetCommand(string command, params string[] args) => command switch
+{
+    _ => () => Console.WriteLine($"Command not found: {command}"),
+};
+
+internal delegate void Command();
